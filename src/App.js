14c1a9d91b0aff61979376d935fa067b67ce6cd5
Dan Faulkner <Home@Dans-iMac.lan>
2021-01-05 20:25:21 +0000

    fix: create dummy question

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
 
+import React, {useState} from 'react';
+
 function App() {
+
+  const [score, setScore] = useState(0)
+
   return (
     <div className="App">
       <header className="App-header">
         <div className="container">
         <h2>My Quiz</h2>
+        <p>Current Score {score}</p>
+
+        <div className="questionContainer">
+          <p>This is the very first question</p>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
+            <label class="form-check-label" for="flexRadioDefault1">
+              Answer 1
+            </label>
+          </div>
+
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
+            <label class="form-check-label" for="flexRadioDefault2">
+              Answer 2
+            </label>
+          </div>
+
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
+            <label class="form-check-label" for="flexRadioDefault3">
+              Answer 3
+            </label>
+          </div>
+
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
+            <label class="form-check-label" for="flexRadioDefault4">
+              Answer 4
+            </label>
+          </div>
+
+        </div>
         </div>
       </header>
     </div>
