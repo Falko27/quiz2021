0fa5eb693feb18bf4d2f5993fd6e2a7ccff8cee4
Dan Faulkner <Home@Dans-iMac.lan>
2021-01-05 20:59:10 +0000

    fix: extracted question to array of objects

--- src/components/question/question.js
+++ src/components/question/question.js
@@ -1,36 +1,45 @@
 import React from 'react';
+import './style.css'
+import QuestionList from './questionList'
+
+//const test = QuestionList[0]
+//console.log(test)
+
+const {questionNumber, question, answer1, answer2, answer3, answer4, correctAnswer} = QuestionList[0]
 
 const Question = () => {
     return (
         <div className="questionContainer">
-          <p>This is the very first question</p>
+          <p>{questionNumber}. {question}</p>
+
           <div class="form-check">
             <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
             <label class="form-check-label" for="flexRadioDefault1">
-              Answer 1
+            {answer1}
             </label>
           </div>
 
           <div class="form-check">
             <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
             <label class="form-check-label" for="flexRadioDefault2">
-              Answer 2
+            {answer2}
             </label>
           </div>
 
           <div class="form-check">
             <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
             <label class="form-check-label" for="flexRadioDefault3">
-              Answer 3
+            {answer3}
             </label>
           </div>
 
           <div class="form-check">
             <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
             <label class="form-check-label" for="flexRadioDefault4">
-              Answer 4
+            {answer4}
             </label>
           </div>
+          <p>The correct answer is {correctAnswer}</p>
         </div>
     )
 }
---
+++ src/components/question/questionList.js
@@ -0,0 +1,12 @@
+const questionList = [
+    {
+        questionNumber : 1,
+        question : 'This is the first question.', 
+        answer1 : 'answer 1', 
+        answer2 : 'answer 2', 
+        answer3 : 'answer 3', 
+        answer4 : 'answer 4', 
+        correctAnswer: 'answer1'
+    }
+]
+export default questionList;
